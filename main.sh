4afa99ee9e2bf2adc1021a1efddc43c7c1108509
Mhamad Dev <124891732+DevMD963@users.noreply.github.com>
2023-10-17 08:45:07 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -8,6 +8,19 @@ colorize() {
     tput sgr0
 }
 
+install_nmap_if_not_exist() {
+    if ! command -v nmap &> /dev/null; then
+        colorize 3 "nmap is not installed. Installing...\n"
+        sudo apt-get update
+        sudo apt-get install nmap -y
+    fi
+}
+
+check_open_ports() {
+    colorize 4 "Checking open ports using nmap...\n"
+    nmap -p 1-65535 localhost
+}
+
 display_server_info() {
     colorize 4 "Hostname: "
     echo "$(hostname)"
@@ -28,5 +41,7 @@ calculate_server_usage() {
 clear
 
 colorize 2 "Byte Syria Security program\n\n"
+install_nmap_if_not_exist
 display_server_info
 calculate_server_usage
+check_open_ports
